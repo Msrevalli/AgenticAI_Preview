d2ccd586b3c43da74157b35c3831cabbb2878597
Msreevalli <sreevalli.manda@gmail.com>
2025-01-29 12:59:44 +0530

    commit

---
+++ monitoring.py
@@ -0,0 +1,10 @@
+from phi.agent import Agent
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+os.environ['PHI_API_KEY']=os.getenv("PHI_API_KEY")
+
+agent = Agent(markdown=True, monitoring=True)
+agent.print_response("Share a 2 sentence horror story")
